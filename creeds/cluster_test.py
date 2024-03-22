7461344e7cd5a54ad96679e0e93f7b457131891d
Luca Conconi <lconconi@ethz.ch>
2024-03-22 15:48:47 +0100

    Add ClusterMaker test in creeds/cluster_test.py

---
+++ creeds/cluster_test.py
@@ -0,0 +1,10 @@
+import unittest
+from cluster_maker import ClusterMaker
+
+class TestCluster(unittest.TestCase):
+    def test_cluster(self):
+        cluster = ClusterMaker("../input/setCD")
+        self.assertIsInstance(cluster, ClusterMaker)
+
+if __name__ == '__main__':
+    unittest.main()
