f82ea02a418c4e2c527ecc2b600c053df03b1fed
Luca Conconi <lconconi@ethz.ch>
2024-04-16 15:47:24 +0200

    Update visualization.py to improve cluster drawing and save the plot

--- creeds/visualization.py
+++ creeds/visualization.py
@@ -1,37 +1,61 @@
+import os
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
-def drawClusters(sub_IDs : map):
+def drawClusters(clustering : map, plotFolder:str):
+    '''
+    Draws Subset 
+    '''
     G = nx.Graph()
-    
+    pos = {}
+    colors = ["blue", "green", "yellow", "orange"] * (len(clustering.keys()) // 4 + 1)
     # Add nodes
-    for cluster in sub_IDs:
-        for i, ligand in enumerate(sub_IDs[cluster]):
-            G.add_node(ligand)
+    off_number = 5
+    for cluster_number, cluster_name in enumerate(clustering.keys()):
+        
+        if cluster_number == 0:
+            offset = (0, 0)
+        elif cluster_number % 4 == 0:
+            offset = (off_number, off_number)
+            off_number += 4
+        elif cluster_number % 4 == 1:
+            offset = (-off_number, off_number)
+        elif cluster_number % 4 == 2:
+            offset = (off_number, -off_number)
+        elif cluster_number % 4 == 3:
+            offset = (-off_number, -off_number)
+
+        dis = 1
+        for i, ligand_1 in enumerate(clustering[cluster_name]):
+            G.add_node(ligand_1)
             if i == 0:
-                first_ligand = ligand
-            else: 
-                G.add_edge(first_ligand, ligand)
-    
-    nx.draw(G)
+                pos[ligand_1] = (offset[0], offset[1])
+            elif i % 4 == 0:
+                pos[ligand_1] = (dis  + offset[0], offset[1])
+                dis += 1
+            elif i % 4 == 1:
+                pos[ligand_1] = (offset[0], dis + offset[1])
+            elif i % 4 == 2:
+                pos[ligand_1] = (-dis + offset[0], offset[1])
+            elif i % 4 == 3:
+                pos[ligand_1] = (offset[0], -dis + offset[1])
+
+            for j, ligand_2 in enumerate(clustering[cluster_name]):
+                
+                if i == j or i < j:
+                    continue
+                else: 
+                    G.add_edge(ligand_1, ligand_2)
+
+    nx.draw(G, pos, node_color = range(len(G.nodes)), cmap = plt.cm.viridis)
     plt.show()
+    saveLoc = os.path.join(plotFolder, "visualization.png")
+    plt.savefig(saveLoc)
 
 if __name__ == "__main__":
     # Test the function
-    sub_IDs = {0: ['mobley_1520842', 
-                   'mobley_1873346', 
-                   'mobley_20524', 
-                   'mobley_3398536', 
-                   'mobley_4287564', 
-                   'mobley_4483973', 
-                   'mobley_4883284', 
-                   'mobley_5977084', 
-                   'mobley_7599023', 
-                   'mobley_7608462'], 
-                1: ['mobley_3187514', 
-                    'mobley_4035953', 
-                    'mobley_9478823']}
-    
-    drawClusters(sub_IDs)
+    clustering = json.load(open('output/FFS/clustersFFS_cluster04_c_MCMS.json', "r"))
+    drawClusters(clustering, "output/FFS/plots/")
     
